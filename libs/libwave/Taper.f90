1080ed9bfbf0f02534bb1c00df4a12c05340b375
Antoine Guitton <aguitton@gmail.com>
2016-09-08 09:35:09 -0600

    adding tapering of images before summation to decrease edge effects

---
+++ libs/libwave/Taper.f90
@@ -0,0 +1,40 @@
+module Taper_mod
+
+  use ModelSpace_types
+
+  implicit none
+
+contains
+
+  subroutine compute_taper(mod)
+    type(ModelSpace) ::    mod
+    
+    real    ::pi=3.14159265359
+
+    allocate(mod%taperx(nxw))
+    allocate(mod%tapery(nyw))
+
+    mod%taperx=1.
+    mod%tapery=1.
+    
+    call cos_sin_taper(mod%taperx,min(mod%nxw/2,10),mod%nxw)
+    if (mod%nyw.gt.1) call cos_sin_taper(mod%tapery,min(mod%nyw/2,10),mod%nyw)
+
+  end subroutine compute_taper
+
+  subroutine cos_sin_taper(array,ntaper,n)
+    real, dimension(:) ::  array
+    integer ::                   ntaper
+    integer ::                          n
+    integer :: i
+    real    :: pi=3.14159265359
+
+    do i=1,min(ntaper,n)
+       array(i)=sin(pi*(i-1)/(2*(min(ntaper,n)-1)))**2
+    end do
+    do i=max(1,n-ntaper),n
+       array(i)=cos(pi*(i-max(1,n-ntaper))/(2*(n-1)))**2
+    end do
+    
+  end subroutine cos_sin_taper
+end module Taper_mod
